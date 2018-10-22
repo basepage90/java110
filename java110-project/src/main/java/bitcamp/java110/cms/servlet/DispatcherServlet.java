19201e38bd4e7170f2aad758f0b594692bdc4411
kwj <woojebiz@gmail.com>
2018-10-22 14:31:07 +0900

    페이지 컨트롤러를 POJO로 전환 완료

--- java110-project/src/main/java/bitcamp/java110/cms/servlet/DispatcherServlet.java
+++ java110-project/src/main/java/bitcamp/java110/cms/servlet/DispatcherServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.context.ApplicationContext;
+
+import bitcamp.java110.cms.web.PageController;
+
 public class DispatcherServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -19,18 +23,39 @@ public class DispatcherServlet extends HttpServlet {
         // 클라이언트가 요청한 URL에서 /app 다음에 지정한 경로를 추출한다.
         String pageControllerPath = request.getPathInfo();
         
-        // 페이지 컨트롤러 실행 
-        RequestDispatcher rd = request.getRequestDispatcher(
-                                            pageControllerPath);
-        rd.include(request, response);
+        // 스프링 IoC 컨테이너 가져오기
+        ApplicationContext iocContainer = 
+                (ApplicationContext)this.getServletContext()
+                                        .getAttribute("iocContainer");
         
-        // 페이지 컨트롤러의 리턴 값 추출
-        String viewUrl = (String)request.getAttribute("viewUrl");
-        
-        // 페이지 컨트롤러가 지정한 URL을 실행
-        response.setContentType("text/html;charset=UTF-8");
-        rd = request.getRequestDispatcher(viewUrl);
-        rd.include(request, response);
+        try {
+            // IoC 컨테이너에서 페이지 컨트롤러를 찾는다.
+            PageController controller = 
+                (PageController) iocContainer.getBean(pageControllerPath);
+            
+            // PageController 실행
+            String viewUrl = controller.service(request, response);
+            
+            if (viewUrl.startsWith("redirect:")) {
+                response.sendRedirect(viewUrl.substring(9));
+                
+            } else {
+                // 페이지 컨트롤러가 지정한 URL을 실행
+                response.setContentType("text/html;charset=UTF-8");
+                RequestDispatcher rd = 
+                        request.getRequestDispatcher(viewUrl);
+                rd.include(request, response);
+            }
+            
+        } catch (Exception e) {
+            request.setAttribute("error", e);
+            request.setAttribute("message", "실행 오류!");
+
+            response.setContentType("text/html;charset=UTF-8");
+            RequestDispatcher rd = 
+                    request.getRequestDispatcher("/error.jsp");
+            rd.include(request, response);
+        }
     }
 }
 
